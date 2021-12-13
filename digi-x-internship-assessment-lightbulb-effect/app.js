976681c4b3ac17917e75d662cc9e22e7957662d5
mrfaeez <faexd99@gmail.com>
2021-12-13 13:22:38 +0800

    ANSWERED LIGHT_BULB

--- digi-x-internship-assessment-lightbulb-effect/app.js
+++ digi-x-internship-assessment-lightbulb-effect/app.js
@@ -1,2 +1,5 @@
+function changeState() {
+    document.getElementById("bulb").classList.toggle('bulb-on'); // use javascript function toggle() to add the 'bulb-on' classname
+}
 
-
+// this is the simplest solution but not the best approach as the class 'bulb-off' is not overwritten
